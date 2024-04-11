9bc1d0910b5fab4754c916a3ee80ecc49e2ea303
Isaac Laffely <laffely.isaac@gmail.com>
2024-04-11 16:13:37 -0600

    Added comments and upaded var names to convention

--- UDPServerFileGBN.c
+++ UDPServerFileGBN.c
@@ -14,7 +14,7 @@
 //structs
 struct dataPacket {
     int type;
-    int seq_no;
+    int seqNo;
     int length;
     char data[512];
 };
@@ -32,10 +32,10 @@ void updateACK (ACKPacket* ack, int type, int num);
 int loosePacketOutOfTen();
 
 int main(void){
-    int socket_desc;
-    struct sockaddr_in server_addr, client_addr;
-    char client_message[BUFF];
-    unsigned int client_struct_length;
+    int socketDesc;
+    struct sockaddr_in serverAddr, clientAddr;
+    char clientMessage[BUFF];
+    unsigned int clientLen;
     int messageSize;
     int seqNum;
     int valid;
@@ -43,24 +43,24 @@ int main(void){
     FILE *outputFile;
 
     //clean server adderss
-    memset(&server_addr, 0, sizeof(server_addr));
+    memset(&serverAddr, 0, sizeof(serverAddr));
 
     // Create UDP socket:
-    socket_desc = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    socketDesc = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
     
-    if(socket_desc < 0){
+    if(socketDesc < 0){
         printf("Error while creating socket\n");
         return -1;
     }
     printf("Socket created successfully\n");
     
     // Set port and IP:
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(PORT);
-    server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(PORT);
+    serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
     
     // Bind to the set port and IP:
-    if(bind(socket_desc, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0){
+    if(bind(socketDesc, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) < 0){
         printf("Binding with port failed\n");
         return -1;
     }
@@ -74,7 +74,7 @@ int main(void){
     int oneTerm = 0;
     while (valid) {
 
-        client_struct_length = sizeof(client_addr);
+        clientLen = sizeof(clientAddr);
 
         dataPacket inboundPacket;
 
@@ -82,31 +82,31 @@ int main(void){
 
 
         //wait until you get a message from the client and store it in inbound packet
-        if ((messageSize = recvfrom(socket_desc, &inboundPacket, sizeof(inboundPacket), 0,
-            (struct sockaddr *) &client_addr, &client_struct_length)) < 0) 
+        if ((messageSize = recvfrom(socketDesc, &inboundPacket, sizeof(inboundPacket), 0,
+            (struct sockaddr *) &clientAddr, &clientLen)) < 0) 
         {
             printf("Receiving failed\n");
             return -1;
         }
-        seqNum = inboundPacket.seq_no;
+        seqNum = inboundPacket.seqNo;
         printf("\n\nMessage Size Received: %d\n",messageSize);
         //schedruled packet loss for testing purposes
         if (!loosePacketOutOfTen()) 
         {
 
             //check if this is the initial packet
-            if (inboundPacket.type == 1 && inboundPacket.seq_no == 0) 
+            if (inboundPacket.type == 1 && inboundPacket.seqNo == 0) 
             {
 
                 //zero out the buffer
-                memset(client_message, '\0', sizeof(client_message));
+                memset(clientMessage, '\0', sizeof(clientMessage));
 
 
                 //transfer the data from the packet to the buffer with a received prefix to distinguish the received file when run locally
-               sprintf(client_message, "received--%s", inboundPacket.data);
+               sprintf(clientMessage, "received--%s", inboundPacket.data);
                 printf("\n\nInitial packet received with filename: %s",inboundPacket.data);
                 //open a file for writing
-                outputFile = fopen(client_message, "w");
+                outputFile = fopen(clientMessage, "w");
 
                 if (outputFile == NULL) {
                     printf("Error opening file\n");
@@ -114,18 +114,18 @@ int main(void){
                 }
 
                 //printing information about the file
-                memset(client_message, '\0', sizeof(client_message));
-                printf("Initial packet received from %s\n", inet_ntoa(client_addr.sin_addr));
+                memset(clientMessage, '\0', sizeof(clientMessage));
+                printf("Initial packet received from %s\n", inet_ntoa(clientAddr.sin_addr));
 
                 fprintf(outputFile, "Initial packet received from %s\n%s\n", 
-                    inet_ntoa(client_addr.sin_addr), client_message);
+                    inet_ntoa(clientAddr.sin_addr), clientMessage);
 
                 base = 0;
                 updateACK(&ACK,inboundPacket.type,base);
 
-            } else if (inboundPacket.type == 2 && inboundPacket.seq_no == base + 1) //if it is not the initial packe, check if it is the correct sequential packet'
+            } else if (inboundPacket.type == 2 && inboundPacket.seqNo == base + 1) //if it is not the initial packe, check if it is the correct sequential packet'
             {
-                printf("Correct packet received. Packet: %d\nPacket Type: %d", inboundPacket.seq_no, inboundPacket.type);
+                printf("Correct packet received. Packet: %d\nPacket Type: %d", inboundPacket.seqNo, inboundPacket.type);
                 printf("\n\nData: -- %s\n\n", inboundPacket.data);
                 //print data to file
                 inboundPacket.data[inboundPacket.length] = '\0';
@@ -136,10 +136,10 @@ int main(void){
                 updateACK(&ACK, inboundPacket.type, base);
                 ackVal = 1;
 
-            } else if (inboundPacket.type == 2 && inboundPacket.seq_no != base + 1) //if it is a data packet but the sequence is wrong
+            } else if (inboundPacket.type == 2 && inboundPacket.seqNo != base + 1) //if it is a data packet but the sequence is wrong
             {
-                printf("Out of sequence packet received. Packet: %d\n", inboundPacket.seq_no);
-                memset(client_message, '\0', sizeof(client_message));
+                printf("Out of sequence packet received. Packet: %d\n", inboundPacket.seqNo);
+                memset(clientMessage, '\0', sizeof(clientMessage));
                 //update ack with num of last in seq packet DO NOT update base
 
                 updateACK(&ACK,inboundPacket.type,base);
@@ -149,8 +149,8 @@ int main(void){
 
             } 
 
-            //check for the terminal packet, which should also be the last 
-            if (inboundPacket.type == 3 && inboundPacket.seq_no == base + 1) {
+            //check for the terminal packet, which should also follow the last data packet
+            if (inboundPacket.type == 3 && inboundPacket.seqNo == base + 1) {
                 
                 //update the base to inactive
                 base = -1;
@@ -162,15 +162,15 @@ int main(void){
 
                 //close the output file
                 fclose(outputFile);
-                memset(client_message, '\0', sizeof(client_message));
+                memset(clientMessage, '\0', sizeof(clientMessage));
                 updateACK(&ACK, inboundPacket.type, base);
             } 
 
             if (base >= 0 && ackVal == 1) //Send ACK for each packet in sequence, do not send ack for out os sequence packets
             {
-                printf("Sending ACK: %d\n Sent to -- %s\nACK type: %d\n", base, inet_ntoa(client_addr.sin_addr),ACK.type);
-                if (sendto(socket_desc, &ACK, sizeof(ACK), 0,
-                (struct sockaddr *) &client_addr, sizeof(client_addr)) != sizeof(ACK)) {
+                printf("Sending ACK: %d\n Sent to -- %s\nACK type: %d\n", base, inet_ntoa(clientAddr.sin_addr),ACK.type);
+                if (sendto(socketDesc, &ACK, sizeof(ACK), 0,
+                (struct sockaddr *) &clientAddr, sizeof(clientAddr)) != sizeof(ACK)) {
                     printf("Incorrect number of bytes sent");
                     return -1;
                 }
@@ -179,8 +179,8 @@ int main(void){
                 //reset base to -2 so that another terminal signal will not be sent until another file is recived
                 base = -2;
                 printf("Terminal signal received, EOF confirmed\n");
-                if (sendto(socket_desc, &ACK, sizeof(ACK), 0,
-                (struct sockaddr *) &client_addr, sizeof(client_addr)) != sizeof(ACK)) {
+                if (sendto(socketDesc, &ACK, sizeof(ACK), 0,
+                (struct sockaddr *) &clientAddr, sizeof(clientAddr)) != sizeof(ACK)) {
                     printf("Incorrect number of bytes sent");
                     return -1;
 
@@ -194,7 +194,7 @@ int main(void){
     //currently this cant be accessed, thinking about adding a kill signal
     printf("\nLoop killed, server shutdown\n");
     // Close the socket
-    close(socket_desc);
+    close(socketDesc);
     
     return 0;
 }
